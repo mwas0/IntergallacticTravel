93e8bf1be25d20460daad8ec072409c6cb4d9431
Maya Wasilewski <mwasilewski3454@stu.d214.org>
2021-11-03 12:41:02 -0500

    Initial Commit

---
+++ IntergallacticTravel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IntergallacticTravel
+//
+//  Created by Maya Wasilewski on 11/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
